88e6213b8f5c752a6d95da8426ef17b8fdbe6c62
Alexander Kurdasov <smq892675@gmail.com>
2025-08-21 03:50:56 +0500

    строковый калькулятор на 2 числа

---
+++ обучение/all.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <String>
+#include <vector>
+using namespace std;
+int main(){
+    while(true){
+        string the_equation;
+        cout <<"input: ";
+        cin >> the_equation;
+        string num1;
+        string num2;
+        char sign;
+        unsigned char cnt = 0;
+        for(int m = 0; m < the_equation.length(); m++){
+            if (the_equation[m] == '@'){
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+        while(cnt + 1 != the_equation.length()){
+            cout << cnt << endl;
+            for(unsigned short i = 0; i < the_equation.length(); i++){
+                if (the_equation[i] == '@'){
+                    continue;
+                }
+                if (the_equation[i] == '+' || the_equation[i] == '/' || the_equation[i] == '*' || the_equation[i] == '-'){
+                    for(unsigned short j = 0; j < i; j++){
+                        num1 += the_equation[j];
+                        the_equation[j] = '@';
+                    }
+                    sign = the_equation[i];
+                    the_equation[i] = '@';
+                    break;
+                }
+            }
+            cout << sign << endl;
+            cout << the_equation << endl;
+            cout << num1 << endl;
+            cout << num2 << endl;
+            for(unsigned short i = 0; i < the_equation.length(); i++){
+                if (the_equation[i] == '@'){
+                    continue;
+                }
+                if (the_equation[i] == '+' || the_equation[i] == '/' || the_equation[i] == '*' || the_equation[i] == '-'){
+                    for(unsigned short j = 0; j < i; j++){
+                        if (the_equation[j] != '@'){
+                            num2 += the_equation[j];
+                            the_equation[j] = '@';
+                        }
+                    }
+                    break;
+                }
+                if (i + 1 == the_equation.length()){
+                   for(unsigned short j = 0; j < i + 1; j++){
+                    if (the_equation[j] != '@'){
+                            num2 += the_equation[j];
+                            the_equation[j] = '@';
+                        }
+                    }
+                    break; 
+                }
+            }
+            cout << num1 << endl;
+            cout << num2 << endl;
+            cout << sign << endl;
+            cout << the_equation << endl;
+            int num_one = std::stoi(num1);
+            int num_two = std::stoi(num2);
+            cout << num_one << endl;
+            cout << num_two << endl;
+            if (sign == '+'){
+            cout << num_one + num_two << endl;
+            }
+            else if(sign == '-'){
+                cout << num_one - num_two << endl;
+            }
+            else if(sign == '/'){
+                cout << num_one / num_two << endl;
+            }
+            else if(sign == '*'){
+                cout << num_one * num_two << endl;
+            }
+            break;
+        }
+        num1 = "";
+        num2 = "";
+        the_equation = "";
+        cnt = 0;
+    }
+
+}
